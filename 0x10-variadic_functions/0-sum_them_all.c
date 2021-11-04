51302059b6e5641eba94a80d3fddb5483f835991
hedaecra <3883@holbertonschool.com>
2021-11-04 06:02:00 +0000

    Sum all parameters

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - calculates the sum of all its parameters
+ * @n: number of arguments passed to the function
+ *
+ * Return: the resulting sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i;
+int sum = 0;
+va_list all;
+
+va_start(all, n);
+
+for (i = 0; i < n; i++)
+sum += va_arg(all, int);
+
+va_end(all);
+
+return (sum);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -74,7 +74,7 @@ int _atoi(char *s);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_numbers(void);
+/*void print_numbers(void);*/
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
