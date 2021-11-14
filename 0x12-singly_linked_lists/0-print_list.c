0895ec4e7b5310034f9863a10f72fe295fe65452
hedaecra <3883@holbertonschool.com>
2021-11-14 00:03:09 +0000

    Update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,15 +11,16 @@
  */
 size_t print_list(const list_t *h)
 {
-unsigned int n;
+size_t n = 0;
 
-for (n = 0; h; n++)
+while (h)
 {
 if (!h->str)
 printf("[0] (nil)\n");
 else
 printf("[%u] %s\n", h->len, h->str);
 
+n++;
 h = h->next;
 }
 return (n);
