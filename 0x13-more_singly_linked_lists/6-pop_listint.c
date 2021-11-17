30e4f5dcb72ab3e347499b8b51e44373d244dc99
hedaecra <3883@holbertonschool.com>
2021-11-17 04:51:15 +0000

    Delete

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Delete linken list
+ * @head: Pointer to first element in the linked list
+ *
+ * Return: Data inside element delete
+ * or 0 if the list is empty
+ */
+int pop_listint(listint_t **head)
+{
+listint_t *tmp;
+int nu;
+
+if (!head || !*head)
+return (0);
+
+nu = (*head)->n;
+tmp = (*head)->next; 
+free(*head);
+*head = tmp;
+return (nu);
+}
