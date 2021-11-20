0460ee804c7f020a35647eededfcb90461a2b5cf
hedaecra <3883@holbertonschool.com>
2021-11-20 01:09:56 +0000

    Update file2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,7 +5,7 @@
  * @n: number to search
  * @index: index of the bit
  *
- * return: value of the bit
+ * Return: value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
@@ -15,5 +15,6 @@ if (index > 63)
 return (-1);
 
 val_idx = (n >> index) & 1;
+
 return (val_idx);
 }
